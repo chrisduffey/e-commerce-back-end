09aa9f36b1b373a30b6f48738b51366ada213514
chrisduffey <duffeyvt12@gmail.com>
2024-05-29 21:59:20 -0400

    added models

--- models/Product.js
+++ models/Product.js
@@ -9,6 +9,32 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTERGER,
+      allowsNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name:{
+      type: DataTypes.STRING,
+      allowsNull: false
+    },
+    price:{
+      type: DataTypes.DECIMAL,
+      allowsNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.DECIMAL,
+      allowsNull: false,
+      defaultValue: 10,
+      validate:{
+        isNumeric: true
+      }
+    },
+   
     // define columns
   },
   {
